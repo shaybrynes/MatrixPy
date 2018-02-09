5bd9cd8c3699b4244a00fa913533faeaf8f40f66
Shay Brynes <shay.brynes@hotmail.co.uk>
2018-02-09 19:25:55 +0000

    Added Transpose methods
    Added the transpose methods to the Matrix class.

--- MatrixPy/matrix.py
+++ MatrixPy/matrix.py
@@ -8,6 +8,7 @@ from MatrixPy.operations.sub import *
 from MatrixPy.operations.mult import *
 from MatrixPy.operations.det import *
 from MatrixPy.operations.inv import *
+from MatrixPy.operations.transpose import *
 
 
 class Matrix:
@@ -74,7 +75,7 @@ class Matrix:
 
         # Convert the outer tuple to a list.
         to_list = list(to_list)
-        
+
         return to_list
 
     @staticmethod
@@ -101,6 +102,30 @@ class Matrix:
         to_tuple = tuple(to_tuple)
 
         return to_tuple
+
+    def ins_transpose(self):
+        """""
+        Finds the transpose of this instance. Replaces the value of the
+        instance with its transpose.
+        
+        """""
+
+        resultant = calc_trans(self.to_list())
+
+        self.matrix = Matrix.to_tuple(resultant)
+
+    @staticmethod
+    def transpose(a):
+        """""
+        Finds the transpose of the matrix a.
+        
+        :return: The matrix that is the transpose of 'a'.
+        :rtype: Matrix
+        """""
+
+        resultant = calc_trans(a.to_list())
+
+        a.matrix = Matrix.to_tuple(resultant)
 
     def ins_add(self, b):
         """""
@@ -355,8 +380,6 @@ class Matrix:
     def ins_inverse(self):
         """""
         Finds the inverse of the instance matrix.
-
-        :return: None, sets the matrix to its inverse.
         """""
 
         # If the Matrix is not singular.
