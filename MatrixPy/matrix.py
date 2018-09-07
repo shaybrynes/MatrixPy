c6799b4db11b4de0fd166edc0da2adf33368c35f
Shay Brynes <shay.brynes@hotmail.co.uk>
2018-09-07 18:29:55 +0100

    Significant structure changes
    Made significant changes to how Matrix objects handle.
    Matrices now have the ability to use Pythons Dunder system.

--- MatrixPy/matrix.py
+++ MatrixPy/matrix.py
@@ -71,11 +71,33 @@ class Matrix:
         self.matrix = Matrix.to_tuple(solution)
 
     @staticmethod
+    def __verify_square__(a):
+        """""
+        Determines if the matrix is square.
+        """""
+
+        matrix_tuple = Matrix.to_tuple(a)
+
+        # Find the number of rows in the matrix.
+        num_rows = len(matrix_tuple)
+
+        # Iterate over the rows.
+        for row in matrix_tuple:
+
+            # If the row has more elements than the number of rows, then we aren't square.
+            if len(row) != num_rows:
+
+                return False
+
+        return True
+
+    @staticmethod
     def __verify_matrix__(matrix_input):
         """""
         Determines if the matrix is rectangular, this is important for many
         of the operations in this module. 
         """""
+
         # Length of the first row.
         first_len = len(matrix_input[0])
 
@@ -205,7 +227,7 @@ class Matrix:
             self.matrix = Matrix.to_tuple(solutions)
 
     @staticmethod
-    def generate(m, n, minimum, maximum, *ignore, integers=True, decimal_places=None):
+    def generate_random(m, n, minimum, maximum, *ignore, integers=True, decimal_places=None):
         """""
         Generate a random matrix of a particular size.
 
@@ -267,16 +289,27 @@ class Matrix:
                 # Throw a TypeError
                 raise TypeError
 
-    def ins_transpose(self):
+    @staticmethod
+    def generate_identity(m):
         """""
-        Finds the transpose of this instance. Replaces the value of the
-        instance with its transpose.
+        Generate an identity matrix of size m.
         
+        :param int m: The size of the identity matrix.
+        :return: Returns an identity matrix of size m.
+        :rtype: Matrix
         """""
 
-        resultant = calc_trans(self.to_list())
+        # Create a list to represent the identity.
+        matrix_list = [[Decimal(0) for i in range(m)] for j in range(m)]
 
-        self.matrix = Matrix.to_tuple(resultant)
+        # Change the diagonal elements to 1.
+        for i in range(m):
+            matrix_list[i][i] = Decimal(1)
+
+        # Create a matrix object.
+        c = Matrix(matrix_list)
+
+        return c
 
     @staticmethod
     def transpose(a):
@@ -288,39 +321,76 @@ class Matrix:
         """""
 
         resultant = calc_trans(a.to_list())
-
         a.matrix = Matrix.to_tuple(resultant)
 
-    def ins_add(self, b):
+        return a
+
+    def __add__(self, other):
         """""
-        Adds another matrix to the instance. Replacing the value of 
-        this instance with the value of the sum.
-
-        :param Matrix b: Is another instance of the Matrix class.
+        Adds two matrices using Python's built in addition capabilities.
+        c = a + b 
+        
+        :return: The addition of the two matrix objects.
+        :rtype: Matrix
         """""
 
-        # If 'b' is of the type Matrix
-        if isinstance(b, Matrix):
+        # Use the add() function.
+        c = self.add(self, other)
 
-            # Add the two matrices together.
-            # Send list versions of the matrix tuples, since tuples are immutable.
-            resultant = calc_add(self.to_list(), b.to_list())
+        return c
 
-            # If there was a problem with the addition.
-            if type(resultant) is str:
-                print(resultant)
-                raise ValueError
+    def __sub__(self, other):
+        """""
+        Subtract two matrices using Python's built in subtraction capabilities.
+        c = a - b 
 
-            # Set the value of this instance to the sum.
-            else:
+        :return: The subtraction of the two matrix objects.
+        :rtype: Matrix
+        """""
 
-                # Since resultant is a list, convert it back to a tuple.
-                self.matrix = Matrix.to_tuple(resultant)
+        # Use the subtract() function.
+        c = self.subtract(self, other)
 
-        else:
-            print("ERROR: Argument 'b' is not of type 'Matrix'.")
-            # Throw a TypeError
-            raise TypeError
+        return c
+
+    def __mul__(self, other):
+        """""
+        Multiplies two matrices using Python's built in multiplication capabilities.
+        c = a * b 
+
+        :return: The multiplication of the two matrix objects.
+        :rtype: Matrix
+        """""
+
+        # Use the multiply() function.
+        c = self.multiply(self, other)
+
+        return c
+
+    def __abs__(self):
+        """""
+        Finds the determinant of the matrix.
+        c = |a|
+        
+        :return: The determinant of the matrix.
+        :rtype: decimal
+        """""
+
+        # Use the determinant function.
+        return Matrix.determinant(self)
+
+    def __pow__(self, power, modulo=None):
+        """""
+        Calculates the power of the matrix.
+        c = a ** power
+        
+        :param int power: An integer to raise the matrix too.
+        :return: Matrix which is the power of the original matrix.
+        :rtype: Matrix
+        """""
+
+        # Use the power() function
+        return Matrix.power(self, power)
 
     @staticmethod
     def add(a, b):
@@ -364,37 +434,6 @@ class Matrix:
 
         else:
             print("ERROR: Argument 'a' is not of type 'Matrix'.")
-            # Throw a TypeError
-            raise TypeError
-
-    def ins_subtract(self, b):
-        """""
-        Adds another matrix to the instance. Replacing the value of 
-        this instance with the value of the negation.
-
-        :param Matrix b: Is another instance of the Matrix class.
-        """""
-
-        # If 'b' is of the type Matrix
-        if isinstance(b, Matrix):
-
-            # Subtract the two matrices.
-            # Send list versions of the matrix tuples, since tuples are immutable.
-            resultant = calc_sub(self.to_list(), b.to_list())
-
-            # If there was a problem with the subtraction.
-            if type(resultant) is str:
-                print(resultant)
-                raise ValueError
-
-            # Set the value of this instance to the subtraction.
-            else:
-
-                # Since resultant is a list, convert it back to a tuple.
-                self.matrix = Matrix.to_tuple(resultant)
-
-        else:
-            print("ERROR: Argument 'b' is not of type 'Matrix'.")
             # Throw a TypeError
             raise TypeError
 
@@ -443,37 +482,6 @@ class Matrix:
             # Throw a TypeError
             raise TypeError
 
-    def ins_multiply(self, b):
-        """""
-        Multiplies another matrix to the instance. Replacing the value of 
-        this instance with the value of the product.
-
-        :param Matrix b: Is another instance of the Matrix class.
-        """""
-
-        # If 'b' is of the type Matrix
-        if isinstance(b, Matrix):
-
-            # Multiply the two matrices.
-            # Send list versions of the matrix tuples, since tuples are immutable.
-            resultant = calc_mult(self.to_list(), b.to_list())
-
-            # If there was a problem with the product.
-            if type(resultant) is str:
-                print(resultant)
-                raise ValueError
-
-            # Set the value of this instance to the product.
-            else:
-
-                # Since resultant is a list, convert it back to a tuple.
-                self.matrix = Matrix.to_tuple(resultant)
-
-        else:
-            print("ERROR: Argument 'b' is not of type 'Matrix'.")
-            # Throw a TypeError
-            raise TypeError
-
     @staticmethod
     def multiply(a, b):
         """""
@@ -519,16 +527,59 @@ class Matrix:
             # Throw a TypeError
             raise TypeError
 
-    def determinant(self):
+    @staticmethod
+    def power(a, power):
         """""
-        Finds the determinant of the instance matrix.
+        Calculates a to the power of power.
+        
+        :param Matrix a: The matrix that is being raised to the power.
+        :param int power: The exponent to raise the matrix by.
+        :return: The matrix raised to the power.
+        :rtype: Matrix
+        """""
+        if type(power) is int:
 
-        :return: The determinant of the instance.
+            # If the power is negative find the inverse first.
+            if power < 0:
+
+                a = Matrix.inverse(a)
+
+            if power != 0:
+
+                # Find the power using iteration.
+                for i in range(0, abs(power)-1):
+
+                    a *= a
+
+                return a
+
+            else:
+                # If the exponent is zero then the result is the identity matrix.
+                return Matrix.generate_identity(len(Matrix.to_list(a)))
+
+        elif type(power) is str:
+
+            # Check to see if the user is looking for the transpose.
+            if power == "T" or power == "t":
+
+                return Matrix.transpose(a)
+
+        else:
+            print("ERROR: argument power is not of type 'Int'")
+            raise TypeError
+
+    @staticmethod
+    def determinant(a):
+        """""
+        Finds the determinant of the parameter matrix.
+
+        :param Matrix a: The matrix whose determinant is to be found.
+        :return: The determinant of the parameter.
         :rtype: float
         """""
 
         # Send a list version of the matrix tuple, since tuples are immutable.
-        resultant = calc_det(self.to_list())
+        resultant = calc_det(a.to_list())
 
         # If there was a problem with the determinant.
         if type(resultant) is str:
@@ -539,35 +590,7 @@ class Matrix:
         else:
 
             # Return the determinant of the instance.
-            return resultant
-
-    def ins_inverse(self):
-        """""
-        Finds the inverse of the instance matrix.
-        """""
-
-        # If the Matrix is not singular.
-        # Send a list version of the matrix tuple.
-        if Matrix.determinant(self) != 0:
-
-            # Send a list version of the matrix tuple, since tuples are immutable.
-            resultant = calc_inv(self.to_list())
-
-            # If there was a problem with the inverse.
-            if type(resultant) is str:
-                print(resultant)
-                raise ValueError
-
-            # Set the instance matrix to the result.
-            else:
-
-                # Since resultant is a list, convert it back to a tuple.
-                self.matrix = Matrix.to_tuple(resultant)
-
-        # There will be no inverse if it is.
-        else:
-            print("INVERSE: Matrix has no inverse, determinant is 0")
-            raise ZeroDivisionError
+            return Decimal(resultant)
 
     @staticmethod
     def inverse(a):
